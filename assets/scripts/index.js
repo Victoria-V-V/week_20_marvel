2448ef9b1849fce9357234f0b43ff85a987ee9c3
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-05-14 23:19:03 +0700

    Added superheroesContent

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,8 +5,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function getSuperheroes() {
     fetch('./assets/scripts/superheroes.json')
         .then(response => response.json())
-        .then(superhero => {
-            console.log(superhero);
+        .then(superheroes => {
+            console.log(superheroes);
+            let superheroesContent = "";
+            for (let superhero of superheroes) {
+                superheroesContent +=
+                    `<div>
+                    <div class = "image__container"><img class="superheroe__image" src = "${superhero.image}"></div>
+                    <div class = "superhero__name">${superhero.name}</div> 
+                    <div>Вселенная: ${superhero.universe}</div> 
+                    <div>Альтер эго: ${superhero.alterego}</div> 
+                    <div>Род деятельности: ${superhero.alterego}</div> 
+                    <div>Друзья: ${superhero.friends}</div> 
+                    <div>Суперсилы: ${superhero.superpower}</div></div>`;
+            }
+            document.querySelector(".superheroesContent").innerHTML = superheroesContent;
         })
         .catch(error => console.log(error));
 }
